1b6387a9d4a0c8ff51f7dc733a3257384464f380
FWdarling <1853204@tongji.edu.cn>
2019-09-20 03:00:45 +0800

    implement the max binary heap

---
+++ Lab/MaxHeap.h
@@ -0,0 +1,108 @@
+
+#include"vector.h"
+
+using std::cout;
+using std::endl;
+
+template<typename T>
+class MaxHeap{
+private:
+    Vector<T> v;
+    int32_t size;
+
+
+// operator < overload needed
+    void fitdown(int32_t ind);
+
+    void fitup(int32_t ind);
+
+public:
+    MaxHeap(){
+        size = 0;
+    };
+    MaxHeap(Vector<T> vt, int32_t s = 0){
+        v = vt;
+        size = vt.get_len();
+    }
+    ~MaxHeap() = default;
+
+    void add(T val);
+
+    void remove(T val);
+
+    int32_t get_index(T val);
+
+    void print();
+};
+
+template<typename T>
+void MaxHeap<T>::fitdown(int32_t ind){
+    if((ind << 1) + 1 >= size) return ;
+    T s1 = v[(ind << 1) + 1], s2 = v[(ind << 1) + 2];
+    if(s2 < s1 && v[ind] < s1){
+        v[(ind << 1) + 1] = v[ind];
+        v[ind] = s1;
+        fitdown((ind << 1) + 1);
+    }else if(s1 < s2 && v[ind] < s2){
+        v[(ind << 1) + 2] = v[ind];
+        v[ind] = s2;
+        fitdown((ind << 1) + 2);
+    }
+}
+
+template<typename T>
+void MaxHeap<T>::fitup(int32_t ind){
+    if(!ind) return;
+    T f = v[(ind - 1) >> 1];
+    if(f < v[ind]){
+        v[(ind - 1) >> 1] = v[ind];
+        v[ind] = f;
+        fitup((ind - 1) >> 1);
+    }
+    return;
+}
+
+template<typename T>
+void MaxHeap<T>::add(T val){
+    v.push_back(val);
+    size++;
+    fitup(size - 1);
+}
+
+template<typename T>
+void MaxHeap<T>::remove(T val){
+    if(!size) {
+        cout << "Heap is empty!" << endl;
+        return;
+    }
+    int32_t ind = get_index(val);
+    if(ind == -1){
+        cout << "Val is not in the heap!" << endl;
+        return;
+    }
+    v[ind] = v[size - 1];
+    size--;
+    fitdown(ind);
+}
+
+template<typename T>
+int32_t MaxHeap<T>::get_index(T val){
+    if(!size){
+        cout << "Heap is empty!" << endl;
+        return -1;
+    }
+    for(int i = 0; i < size; i++){
+        if(v[i] == val) return i;
+    }
+    return -1;
+}
+
+template<typename T>
+void MaxHeap<T>::print(){
+    if(!size){
+        cout << "Heap is empty!" << endl;
+    }
+    for(int i = 0; i < size; i++){
+        cout << i << ":" << v[i] << endl;
+    }
+}
--- Source/test.cpp
+++ Source/test.cpp
@@ -1,13 +1,22 @@
-#include"q1.h"
+#include"../Lab/MaxHeap.h"
 
 using std::cout;
 using std::endl;
 
 int main(){
-    StudentList sl;
-    Student s("1853204", 18, "Darling", "Male", "Student");
-    sl.Add("1719111", 12, "Lucy Brown", "Female", "Teacher");
-    sl.sl.Add(s);
-    sl.Display();
+    MaxHeap<int> mh;
+    mh.print();
+    mh.add(5);
+    mh.add(-7);
+    mh.print();
+    for(int i = 0; i < 5; i++){
+        mh.add(i);
+    }
+    mh.print();
+    mh.get_index(5);
+    mh.remove(5);
+    mh.print();
+    mh.remove(-7);
+    mh.print();
     return 0;
 }
